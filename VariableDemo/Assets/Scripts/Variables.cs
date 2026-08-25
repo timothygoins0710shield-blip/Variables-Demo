ba46e872663e9e7267f2b74ea06d5a6357f8b979
timothygoins0710shield-blip <timothygoins0710shield@gmail.com>
2026-08-24 21:40:15 -0400

    Variables Demo Assignment

---
+++ VariableDemo/Assets/Scripts/Variables.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    private int score = 20; //question 1
+
+    public string devilDino; //question 2
+
+    private string message;
+    
+    // class level
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //local level 
+
+        string decision = "choice"; //question 3
+        
+        string firstName = "Timothy"; //question 4
+
+        string lastName = "Goins";
+
+        message = "Marvel for Life";
+
+
+
+
+
+    }
+
+}
